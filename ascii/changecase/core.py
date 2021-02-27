160d804154971a1037954b81a8e49c1bbf17810e
Sergey Andreev <andrejew.sw@gmail.com>
2021-02-27 21:01:06 +0300

    Removed body of functions

--- ascii/changecase/core.py
+++ ascii/changecase/core.py
@@ -1,20 +1,2 @@
-import __future__
-
-BIG_A_CODE = ord('A')
-SMALL_A_CODE = ord('a')
-SMALL_Z_CODE = ord('z')
-
 def asciiToUpperCase(string: str) -> str:
-    result = []
-
-    for char in string:
-        currentCharCode = ord(char)
-
-        if (SMALL_A_CODE <= currentCharCode <= SMALL_Z_CODE):
-            code = BIG_A_CODE + currentCharCode - SMALL_A_CODE
-            result.append(chr(code))
-        else:
-            result.append(char)
-
-    return ''.join(result)
-
+    return ''
